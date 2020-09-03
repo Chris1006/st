8dfd2772d9a9426a3efa3b13950744158a269795
christian poß <christianposs@gmx.de>
2020-09-03 10:31:41 +0200

    changed config

--- config.h
+++ config.h
@@ -90,7 +90,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
- float alpha = 0.95;
+ float alpha = 0.9;
 
 static const char *colorname[] = {
     "#282828", /* base00 */
