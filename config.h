0b77ac6832bdb1299c09d7f716353a9c662d0f41
christian poß <christianposs@gmx.de>
2020-12-07 11:46:12 +0100

    changed theme to real gruvbox dark

--- config.h
+++ config.h
@@ -97,28 +97,26 @@ unsigned int tabspaces = 8;
 float alpha = 0.85;
 
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+"#1d2021", /* black   */
+"#fb4934", /* red     */
+"#b8bb26", /* green   */
+"#fabd2f", /* yellow  */
+"#83a598", /* blue    */
+"#d3869b", /* magenta */
+"#8ec07c", /* cyan    */
+"#d5c4a1", /* white   */
+
+"#665c54", /* black   */
+"#fb4934", /* red     */
+"#b8bb26", /* green   */
+"#fabd2f", /* yellow  */
+"#83a598", /* blue    */
+"#d3869b", /* magenta */
+"#8ec07c", /* cyan    */
+"#fbf1c7", /* white   */
+
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
 };
 
 /*
@@ -126,9 +124,9 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 7;
+static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
