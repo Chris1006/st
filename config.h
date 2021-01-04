7b8c49116ca304dc46dba77ef97d4ed4c0109bea
christian poß <christianposs@gmx.de>
2021-01-04 22:35:12 +0100

    changed theme to nord

--- config.h
+++ config.h
@@ -96,37 +96,40 @@ unsigned int tabspaces = 8;
 /* bg opacity */
 float alpha = 0.85;
 
-static const char *colorname[] = {
-"#1d2021", /* black   */
-"#fb4934", /* red     */
-"#b8bb26", /* green   */
-"#fabd2f", /* yellow  */
-"#83a598", /* blue    */
-"#d3869b", /* magenta */
-"#8ec07c", /* cyan    */
-"#d5c4a1", /* white   */
+const char *colorname[] = {
 
-"#665c54", /* black   */
-"#fb4934", /* red     */
-"#b8bb26", /* green   */
-"#fabd2f", /* yellow  */
-"#83a598", /* blue    */
-"#d3869b", /* magenta */
-"#8ec07c", /* cyan    */
-"#fbf1c7", /* white   */
+  /* 8 normal colors */
+  [0] = "#2E3440", /* black   */
+  [1] = "#88C0D0", /* red     */
+  [2] = "#BF616A", /* green   */
+  [3] = "#5E81AC", /* yellow  */
+  [4] = "#EBCB8B", /* blue    */
+  [5] = "#A3BE8C", /* magenta */
+  [6] = "#D08770", /* cyan    */
+  [7] = "#E5E9F0", /* white   */
 
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
+  /* 8 bright colors */
+  [8]  = "#4C566A",  /* black   */
+  [9]  = "#4BADBA",  /* red     */
+  [10] = "#DC6069", /* green   */
+  [11] = "#57A5ED", /* yellow  */
+  [12] = "#E1B86F", /* blue    */
+  [13] = "#8DBB6E", /* magenta */
+  [14] = "#D08770", /* cyan    */
+  [15] = "#8FBCBB", /* white   */
+
+  /* special colors */
+  [256] = "#2E3440", /* background */
+  [257] = "#E5E9F0", /* foreground */
+  [258] = "#E5E9F0",     /* cursor */
 };
 
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 7;
-static unsigned int defaultrcs = 0;
+/* Default colors (colorname index)
+ * foreground, background, cursor */
+ unsigned int defaultbg = 0;
+ unsigned int defaultfg = 257;
+ unsigned int defaultcs = 258;
+ unsigned int defaultrcs= 258;
 
 /*
  * Default shape of cursor
